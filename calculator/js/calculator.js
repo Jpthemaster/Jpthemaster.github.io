9f3a2edb3b322b616a9ce4ab03d4013885c06f42
Jpthemaster <Jpthemaster@icloud.com>
2023-03-01 18:54:01 -0500

    calculator

---
+++ calculator/js/calculator.js
@@ -0,0 +1,9 @@
+console.log("I am here");
+let result = document.getElementById("result");
+
+function displayTwoPlusTwo() {
+    let number1 = 2;
+    let number2 = 2;
+    result.innerHTML = number1 + number2;
+}
+
