b60a1fd6c21eca8b870e8632c35736df66942692
Jpthemaster <Jpthemaster@icloud.com>
2023-03-01 19:13:54 -0500

    modulus

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -7,3 +7,16 @@ function displayTwoPlusTwo() {
     result.innerHTML = number1 + number2;
 }
 
+function displayThreeTimesFour() {
+    result.innerHTML = 3 * 4;
+}
+
+function displaySixDividedByTwo() {
+    let number1 = 6;
+    let number2 = 2;
+    result.innerHTML = number1 / number2;
+}
+
+function displayTenModulusThree() {
+    result.innerHTML = 10 % 3;
+}
