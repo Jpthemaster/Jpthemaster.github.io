902f08edaabce261bcd50026aed1bd8d06da7fa6
Jpthemaster <Jpthemaster@icloud.com>
2023-03-01 22:08:49 -0500

    lightbox javascript

---
+++ lightboxjs/js/lightbox.js
@@ -0,0 +1,19 @@
+let lightboxContainer = document.getElementById("lightboxContainer");
+let lightbocBackground = document.getElementById("lightboxBackground");
+let lightbox = document.getElementById("lightbox");
+let lightboxCloser = document.getElementById("lightboxCloser");
+
+function openLightbox() {
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage = document.getElementById("coffeeImage");
+coffeeImage.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
