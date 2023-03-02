e2ec8b2dd8b2fd3bc628d709b91c68230d744a48
Jpthemaster <Jpthemaster@icloud.com>
2023-03-01 22:35:26 -0500

    gallery1

--- lightboxjs/js/lightbox.js
+++ lightboxjs/js/lightbox.js
@@ -10,6 +10,26 @@ function openLightbox() {
 
 let coffeeImage = document.getElementById("coffeeImage");
 coffeeImage.onclick = openLightbox;
+let coffeeImage1 = document.getElementById("coffeeImage1");
+coffeeImage1.onclick = openLightbox;
+let coffeeImage2 = document.getElementById("coffeeImage2");
+coffeeImage2.onclick = openLightbox;
+let coffeeImage3 = document.getElementById("coffeeImage3");
+coffeeImage3.onclick = openLightbox;
+let coffeeImage4 = document.getElementById("coffeeImage4");
+coffeeImage4.onclick = openLightbox;
+let coffeeImage5 = document.getElementById("coffeeImage5");
+coffeeImage5.onclick = openLightbox;
+let coffeeImage6 = document.getElementById("coffeeImage6");
+coffeeImage6.onclick = openLightbox;
+let coffeeImage7 = document.getElementById("coffeeImage7");
+coffeeImage7.onclick = openLightbox;
+let coffeeImage8 = document.getElementById("coffeeImage8");
+coffeeImage8.onclick = openLightbox;
+let coffeeImage9 = document.getElementById("coffeeImage9");
+coffeeImage9.onclick = openLightbox;
+let coffeeImage10 = document.getElementById("coffeeImage10");
+coffeeImage10.onclick = openLightbox;
 
 function closeLightbox() {
     lightboxContainer.classList.remove("display");
