611018bb0a8c50035830a26115e5825073e479fb
Jpthemaster <Jpthemaster@icloud.com>
2023-03-03 21:41:42 -0500

    date

--- lightboxjs/js/lightbox.js
+++ lightboxjs/js/lightbox.js
@@ -8,28 +8,28 @@ function openLightbox() {
     lightbox.src = this.src;
 }
 
-let grim = document.getElementById("grim");
-grim.onclick = openLightbox;
-let welcome = document.getElementById("welcome");
-welcome.onclick = openLightbox;
-let extract = document.getElementById("extract");
-extract.onclick = openLightbox;
-let rainbow = document.getElementById("rainbow");
-rainbow.onclick = openLightbox;
-let squadDeep = document.getElementById("squadDeep");
-squadDeep.onclick = openLightbox;
-let trio = document.getElementById("trio");
-trio.onclick = openLightbox;
-let arcadeLogo = document.getElementById("arcadeLogo");
-arcadeLogo.onclick = openLightbox;
-let solis = document.getElementById("solis");
-solis.onclick = openLightbox;
-let zero = document.getElementById("zero");
-zero.onclick = openLightbox;
-let alibi = document.getElementById("alibi");
-alibi.onclick = openLightbox;
-let azami = document.getElementById("azami");
-azami.onclick = openLightbox;
+// let grim = document.getElementById("grim");
+// grim.onclick = openLightbox;
+// let welcome = document.getElementById("welcome");
+// welcome.onclick = openLightbox;
+// let extract = document.getElementById("extract");
+// extract.onclick = openLightbox;
+// let rainbow = document.getElementById("rainbow");
+// rainbow.onclick = openLightbox;
+// let squadDeep = document.getElementById("squadDeep");
+// squadDeep.onclick = openLightbox;
+// let trio = document.getElementById("trio");
+// trio.onclick = openLightbox;
+// let arcadeLogo = document.getElementById("arcadeLogo");
+// arcadeLogo.onclick = openLightbox;
+// let solis = document.getElementById("solis");
+// solis.onclick = openLightbox;
+// let zero = document.getElementById("zero");
+// zero.onclick = openLightbox;
+// let alibi = document.getElementById("alibi");
+// alibi.onclick = openLightbox;
+// let azami = document.getElementById("azami");
+// azami.onclick = openLightbox;
 
 function closeLightbox() {
     lightboxContainer.classList.remove("display");
@@ -49,4 +49,4 @@ let galleryImages = document.getElementsByClassName('galleryImage');
         for (let i = 0; i < closers.length; i++) {
             let closer = closers[i];
             closer.onclick = closeLightbox;
-        }
+    }
